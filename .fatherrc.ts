f48cba545839e09f6929bd78fd6ed35267bc50c8
mochamadyudi <myudisobari12@gmail.com>
2025-08-30 18:59:04 +0700

    chore: ignore file in folder demo for including build

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,28 +1,28 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  esm: {
-    input: 'components',
-    output: 'es',
-    platform: 'browser',
-    transformer: 'babel',
-    // ignores: ['**/demo/**', '**/__tests__/**'],
-  },
-  cjs: {
-    input: 'components',
-    output: 'lib',
-    platform: 'browser',
-    transformer: 'babel',
-    // ignores: ['**/demo/**', '**/__tests__/**'],
-  },
-  extraBabelPlugins: [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: 'cloneui',
-        libraryDirectory: '',
-        style: true,
-      },
-    ],
-  ],
+	esm: {
+		input: 'components',
+		output: 'es',
+		platform: 'browser',
+		transformer: 'babel',
+		ignores: ['**/demo/**', '**/__tests__/**'],
+	},
+	cjs: {
+		input: 'components',
+		output: 'lib',
+		platform: 'browser',
+		transformer: 'babel',
+		ignores: ['**/demo/**', '**/__tests__/**'],
+	},
+	extraBabelPlugins: [
+		[
+			'babel-plugin-import',
+			{
+				libraryName: 'cloneui',
+				libraryDirectory: '',
+				style: true,
+			},
+		],
+	],
 });
