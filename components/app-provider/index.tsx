7ac69f38a98faa3288393e2c7f70fd680ab35942
mochamadyudi <myudisobari12@gmail.com>
2025-08-30 18:59:22 +0700

    chore: reformat app-provider

--- components/app-provider/index.tsx
+++ components/app-provider/index.tsx
@@ -5,120 +5,123 @@ import useMemo from 'rc-util/lib/hooks/useMemo';
 import { DesignTokenContext, DesignTokenProviderProps } from '../theme/context';
 import defaultSeedToken from '../theme/themes/seed';
 import {
-  AppConsumerProps,
-  AppContext,
-  ButtonConfig,
-  SpinConfig,
-  ThemeConfig,
+	AppConsumerProps,
+	AppContext,
+	ButtonConfig,
+	SpinConfig,
+	ThemeConfig,
 } from './context';
 import { SizeType } from './interface';
 
 export type {
-  SizeType,
-  AppConsumerProps,
-  AppContext,
-  ButtonConfig,
-  SpinConfig,
+	SizeType,
+	AppConsumerProps,
+	AppContext,
+	ButtonConfig,
+	SpinConfig,
 };
 
 export interface AppProviderProps {
-  getTargetContainer?: () => HTMLElement | Window;
-  prefixCls?: string;
-  children?: React.ReactNode;
-  theme?: ThemeConfig;
+	getTargetContainer?: () => HTMLElement | Window;
+	prefixCls?: string;
+	children?: React.ReactNode;
+	theme?: ThemeConfig;
 
-  spin?: SpinConfig;
-  button?: ButtonConfig;
+	spin?: SpinConfig;
+	button?: ButtonConfig;
 }
 
 interface AppProviderChildrenProps extends AppProviderProps {
-  parentContext: AppConsumerProps;
+	parentContext: AppConsumerProps;
 }
 
 const AppProviderChildren: React.FC<AppProviderChildrenProps> = (props) => {
-  const { children, parentContext, theme } = props;
+	const { children, parentContext, theme } = props;
 
-  // ================================= Context =================================
-  // const getPrefixCls = React.useCallback(
-  //   (suffixCls?: string, customizePrefixCls?: string) => {
-  //     const { prefixCls } = props;
-  //     if (customizePrefixCls) {
-  //       return customizePrefixCls;
-  //     }
-  //     const mergedPrefixCls = prefixCls || parentContext.getPrefixCls('');
-  //     return suffixCls ? `${mergedPrefixCls}-${suffixCls}` : mergedPrefixCls;
-  //   },
-  //   [parentContext.getPrefixCls, props.prefixCls]
-  // );
+	// ================================= Context =================================
+	// const getPrefixCls = React.useCallback(
+	//   (suffixCls?: string, customizePrefixCls?: string) => {
+	//     const { prefixCls } = props;
+	//     if (customizePrefixCls) {
+	//       return customizePrefixCls;
+	//     }
+	//     const mergedPrefixCls = prefixCls || parentContext.getPrefixCls('');
+	//     return suffixCls ? `${mergedPrefixCls}-${suffixCls}` : mergedPrefixCls;
+	//   },
+	//   [parentContext.getPrefixCls, props.prefixCls]
+	// );
 
-  const config: AppConsumerProps = {
-    ...parentContext,
-  };
+	const config: AppConsumerProps = {
+		...parentContext,
+	};
 
-  const memConfig = useMemo(
-    () => config,
-    config,
-    (prev, current) => {
-      const prevKeys = Object.keys(prev) as Array<keyof typeof config>;
-      const currentKeys = Object.keys(current) as Array<keyof typeof config>;
+	const memConfig = useMemo(
+		() => config,
+		config,
+		(prev, current) => {
+			const prevKeys = Object.keys(prev) as Array<keyof typeof config>;
+			const currentKeys = Object.keys(current) as Array<keyof typeof config>;
 
-      return (
-        prevKeys.length !== currentKeys.length ||
-        prevKeys.some((key) => prev[key] !== current[key])
-      );
-    }
-  );
+			return (
+				prevKeys.length !== currentKeys.length ||
+				prevKeys.some((key) => prev[key] !== current[key])
+			);
+		}
+	);
 
-  const mergedTheme = { ...theme, ...parentContext?.theme };
+	const mergedTheme = { ...theme, ...parentContext?.theme };
 
-  const memoTheme = React.useMemo(() => {
-    const { algorithm, token, components, ...rest } = mergedTheme || {};
-    const parsedComponents: any = { ...components };
-    const mergedToken = {
-      ...defaultSeedToken,
-      ...token,
-    };
+	const memoTheme = React.useMemo(() => {
+		const { algorithm, token, components, ...rest } = mergedTheme || {};
+		const parsedComponents: any = { ...components };
+		const mergedToken = {
+			...defaultSeedToken,
+			...token,
+		};
 
-    return {
-      ...rest,
-      token: mergedToken,
-      components: parsedComponents,
-      override: {
-        override: mergedToken,
-        ...parsedComponents,
-      },
-      cssVar: true,
-    } as unknown as DesignTokenProviderProps;
-  }, [mergedTheme]) as DesignTokenProviderProps;
+		return {
+			...rest,
+			token: mergedToken,
+			components: parsedComponents,
+			override: {
+				override: mergedToken,
+				...parsedComponents,
+			},
+			cssVar: true,
+		} as unknown as DesignTokenProviderProps;
+	}, [mergedTheme]) as DesignTokenProviderProps;
 
-  let childNode = <>{children}</>;
+	let childNode = <>{children}</>;
 
-  if (theme) {
-    childNode = (
-      <DesignTokenContext.Provider value={{ ...memoTheme }}>
-        {childNode}
-      </DesignTokenContext.Provider>
-    );
-  }
+	if (theme) {
+		childNode = (
+			<DesignTokenContext.Provider value={{ ...memoTheme }}>
+				{childNode}
+			</DesignTokenContext.Provider>
+		);
+	}
 
-  return (
-    <AppContext.Provider value={memConfig}>
-      <StyleContextProvider>{childNode}</StyleContextProvider>
-    </AppContext.Provider>
-  );
+	return (
+		<AppContext.Provider value={memConfig}>
+			<StyleContextProvider>{childNode}</StyleContextProvider>
+		</AppContext.Provider>
+	);
 };
 
 const AppProvider: React.FC<AppProviderProps> & {
-  /** @private **/
-  Context: typeof AppContext;
+	/** @private **/
+	Context: typeof AppContext;
 } = (props) => {
-  const ctx = React.useContext<AppConsumerProps>(AppContext);
-  return <AppProviderChildren parentContext={ctx} {...props} />;
+	const ctx = React.useContext<AppConsumerProps>(AppContext);
+	return <AppProviderChildren parentContext={ctx} {...props} />;
 };
 
 AppProvider.Context = AppContext;
 
 if (process.env.NODE_ENV !== 'production') {
-  AppProvider.displayName = 'AppProvider';
+	AppProvider.displayName = 'AppProvider';
 }
+
+export { DirectionType, ThemeConfig, AppComponentStyle } from './context';
+
 export default AppProvider;
